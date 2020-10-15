4a5be75919c4b4e0493745e01499594a3f3cd3f4
Tomislav <haxcontroler@gmail.com>
2020-10-15 02:10:25 +0200

    Added javascript file that copies data from <a> tag into search

--- public/assets/js/tkeskic.js
+++ public/assets/js/tkeskic.js
@@ -1,5 +1,6 @@
 function copyDataToInputTag(counter){
     var aTag = document.getElementsByClassName('search-item')[counter];
-    var searchInput = document.getElementById('search_text').value = aTag.childNodes[1].textContent;
+    var text = aTag.childNodes[1].textContent.trim();
+    var searchInput = document.getElementById('search_text').value = text;
     var searchButton = document.getElementById('search_submit').click();
 }
