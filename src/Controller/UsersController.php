187100da70a6c4ed4688f4f70673c13025cd0147
Tomislav <haxcontroler@gmail.com>
2020-11-09 19:11:36 +0100

    Updated Users Controller, added profile rout

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,9 +4,11 @@
 namespace App\Controller;
 
 
+use App\Form\FollowType;
 use App\Service\HistoryService;
 use App\Service\PostService;
 use App\Service\UserService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -51,12 +53,46 @@ class UsersController extends BaseController
     /**
      * @Route("/profile/{id}", name="app_profile")
      * @param $id
+     * @param Request $request
      * @return Response
      */
-    public function profile($id)
+    public function profile($id, Request $request)
     {
+        $alreadyFollowing = false;
+
+
+        //variable that helps us for checking if loggedIn user follows target user
+        $currentUser = $this->getUser();
+
         $user = $this->userService->getUser($id);
-        return $this->render('profile.html.twig', ['user' => $user]);
+
+        $usersAlreadyFollowing = $user->getFollowers();
+
+        if($usersAlreadyFollowing->contains($currentUser)){
+            //user is already following target user
+            $alreadyFollowing = true;
+        }
+
+
+        $followers = $this->userService->getFollowers($user);//this returns users that I follow
+        $following = $this->userService->getFollowings($user);//this returns users that follow me
+        $posts = $this->postService->getPostsFromUser($id);
+        $numberOfFollowers = count($followers);
+        $numberOfFollowings = count($following);
+        $numberOfPosts = count($posts);
+
+        return $this->render(
+            'profile.html.twig',
+            [
+                'following' => $alreadyFollowing,
+                'user' => $user,
+                'numberOfPosts' => $numberOfPosts,
+                'followers' => $followers,
+                'numberOfFollowings' => $numberOfFollowings,
+                'numberOfFollowers' => $numberOfFollowers,
+                'posts' => $posts
+            ]
+        );
     }
 
 }
